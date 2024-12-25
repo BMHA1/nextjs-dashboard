b8424f147a9970be3e9a46dbb9d705ef3216dfc2
Brajin Hanna <bhanna@somosexperiences.com>
2024-12-25 14:02:39 +0100

    crear componente paginacion y busqueda

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -134,6 +134,7 @@ export async function fetchInvoicesPages(query: string) {
   `;
 
     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    console.log('fetchInvoicesPage-totalPages', totalPages);
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
