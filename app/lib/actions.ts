f827a769a82db6e0b58ae56547e00cc4b091a9aa
Brajin Hanna <bhanna@somosexperiences.com>
2024-12-25 14:09:38 +0100

    creación del formulario, funciones "actions" en el servidor y funcionalidad para crear facturas

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,8 @@
 'use server';
 import {z} from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -17,4 +20,17 @@ export async function createInvoice(formData: FormData) {
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+
+    //o llamada a la API
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+   revalidatePath('/dashboard/invoices');
+   redirect('/dashboard/invoices');
 }
